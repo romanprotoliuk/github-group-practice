533ebac5d0d869768bc4c4601e5b17582b0d4afb
romanprotoliuk <romanprotoliuk@gmail.com>
2022-03-23 16:47:18 -0700

    merge fix

--- src/App.js
+++ src/App.js
@@ -3,31 +3,23 @@ import './App.css';
 import Teammate from './components/Teammate';
 
 const teamates = [
-  {
-      name: "Roman",
-      about: "asdasdasdkkjkalsjdlkjasld",
-      skills: 'software'
-  },
-  {
-    name: "Mark Aguilar",
-    about: "sadkjfhsad fsdkfksdjl",
-    skills: "react"
-  }
-]
+	{
+		name: 'Roman',
+		about: 'asdasdasdkkjkalsjdlkjasld',
+		skills: 'software'
+	},
+	{
+		name: 'Mark Aguilar',
+		about: 'sadkjfhsad fsdkfksdjl',
+		skills: 'react'
+	}
+];
 
 function App() {
-  const displayTeam = teamates.map((teamate, index) => {
-    return (
-      <Teammate 
-        teamate={teamate}
-      />
-    )
-  })
-  return (
-    <div className="App">
-      {displayTeam}
-    </div>
-  );
+	const displayTeam = teamates.map((teamate, index) => {
+		return <Teammate teamate={teamate} />;
+	});
+	return <div className="App">{displayTeam}</div>;
 }
 
 export default App;
